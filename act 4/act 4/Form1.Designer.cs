681a4cceff78ef150138110efd28f9b173234fa0
jesusalberto095 <albertojesus095@gmail.com>
2025-02-19 11:37:17 -0600

    Add files via upload

---
+++ act 4/act 4/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace act_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelusuario = new System.Windows.Forms.Label();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.textUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelusuario
+            // 
+            this.labelusuario.AutoSize = true;
+            this.labelusuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelusuario.Location = new System.Drawing.Point(262, 173);
+            this.labelusuario.Name = "labelusuario";
+            this.labelusuario.Size = new System.Drawing.Size(119, 32);
+            this.labelusuario.TabIndex = 0;
+            this.labelusuario.Text = "Usuario";
+            this.labelusuario.Click += new System.EventHandler(this.labelusuario_Click);
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonShow.Location = new System.Drawing.Point(258, 367);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(113, 50);
+            this.buttonShow.TabIndex = 1;
+            this.buttonShow.Text = "show";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // textUsuario
+            // 
+            this.textUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textUsuario.Location = new System.Drawing.Point(258, 224);
+            this.textUsuario.Name = "textUsuario";
+            this.textUsuario.Size = new System.Drawing.Size(123, 30);
+            this.textUsuario.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "password";
+            // 
+            // textPassword
+            // 
+            this.textPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPassword.Location = new System.Drawing.Point(258, 306);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(123, 30);
+            this.textPassword.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 450);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textUsuario);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.labelusuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelusuario;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.TextBox textUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textPassword;
+    }
+}
+
---
+++ act 4/act 4/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace act_4
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void labelusuario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            if(textUsuario.Text== "jesus"&& textPassword.Text == "jesus")
+            {
+                Form2 newfrom2 = new Form2();
+                newfrom2.Show();
+            }
+            else
+            {
+                MessageBox.Show("El usuario o contraseña no coinciden", "error");
+            }
+        }
+    }
+}
