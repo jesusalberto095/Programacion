abe0e73cda3db8401b4f10846cb1caa3c899fe40
jesusalberto095 <albertojesus095@gmail.com>
2025-02-26 12:27:57 -0600

    Add files via upload

---
+++ examen/examen/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace examen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxMes = new System.Windows.Forms.ComboBox();
+            this.comboBoxDias = new System.Windows.Forms.ComboBox();
+            this.buttonOPTENER = new System.Windows.Forms.Button();
+            this.textBoxZodi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxMes
+            // 
+            this.comboBoxMes.BackColor = System.Drawing.Color.GreenYellow;
+            this.comboBoxMes.FormattingEnabled = true;
+            this.comboBoxMes.Location = new System.Drawing.Point(52, 55);
+            this.comboBoxMes.Name = "comboBoxMes";
+            this.comboBoxMes.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxMes.TabIndex = 0;
+            this.comboBoxMes.SelectedIndexChanged += new System.EventHandler(this.comboBoxsig_SelectedIndexChanged);
+            // 
+            // comboBoxDias
+            // 
+            this.comboBoxDias.BackColor = System.Drawing.Color.GreenYellow;
+            this.comboBoxDias.FormattingEnabled = true;
+            this.comboBoxDias.Location = new System.Drawing.Point(363, 55);
+            this.comboBoxDias.Name = "comboBoxDias";
+            this.comboBoxDias.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxDias.TabIndex = 1;
+            // 
+            // buttonOPTENER
+            // 
+            this.buttonOPTENER.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOPTENER.Location = new System.Drawing.Point(173, 135);
+            this.buttonOPTENER.Name = "buttonOPTENER";
+            this.buttonOPTENER.Size = new System.Drawing.Size(158, 67);
+            this.buttonOPTENER.TabIndex = 2;
+            this.buttonOPTENER.Text = "Obtener signo zodiacal";
+            this.buttonOPTENER.UseVisualStyleBackColor = true;
+            this.buttonOPTENER.Click += new System.EventHandler(this.buttonOPTENER_Click);
+            // 
+            // textBoxZodi
+            // 
+            this.textBoxZodi.BackColor = System.Drawing.Color.Lime;
+            this.textBoxZodi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxZodi.Location = new System.Drawing.Point(86, 299);
+            this.textBoxZodi.Name = "textBoxZodi";
+            this.textBoxZodi.Size = new System.Drawing.Size(371, 27);
+            this.textBoxZodi.TabIndex = 3;
+            this.textBoxZodi.TextChanged += new System.EventHandler(this.textBoxZodi_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(545, 450);
+            this.Controls.Add(this.textBoxZodi);
+            this.Controls.Add(this.buttonOPTENER);
+            this.Controls.Add(this.comboBoxDias);
+            this.Controls.Add(this.comboBoxMes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxMes;
+        private System.Windows.Forms.ComboBox comboBoxDias;
+        private System.Windows.Forms.Button buttonOPTENER;
+        private System.Windows.Forms.TextBox textBoxZodi;
+    }
+}
+
---
+++ examen/examen/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace examen
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxMes.Items.Add("ENERO");
+            comboBoxMes.Items.Add("FEBRERO");
+            comboBoxMes.Items.Add("MARZO");
+            comboBoxMes.Items.Add("ABRIL");
+            comboBoxMes.Items.Add("MAYO");
+            comboBoxMes.Items.Add("JUNIO");
+            comboBoxMes.Items.Add("JULIO");
+            comboBoxMes.Items.Add("AGOSTO");
+            comboBoxMes.Items.Add("SEPTIEMBRE");
+            comboBoxMes.Items.Add("OCTUBRE");
+            comboBoxMes.Items.Add("NOVIEMBRE");
+            comboBoxMes.Items.Add("DICIEMBRE");
+            //aqui de clare los meses para el comboboxdel mes 
+
+
+            comboBoxDias.Items.Add("01");
+            comboBoxDias.Items.Add("02");
+            comboBoxDias.Items.Add("03");
+            comboBoxDias.Items.Add("04");
+            comboBoxDias.Items.Add("05");
+            comboBoxDias.Items.Add("06");
+            comboBoxDias.Items.Add("07");
+            comboBoxDias.Items.Add("08");
+            comboBoxDias.Items.Add("09");
+            comboBoxDias.Items.Add("10");
+            comboBoxDias.Items.Add("11");
+            comboBoxDias.Items.Add("12");
+            comboBoxDias.Items.Add("13");
+            comboBoxDias.Items.Add("14");
+            comboBoxDias.Items.Add("15");
+            comboBoxDias.Items.Add("16");
+            comboBoxDias.Items.Add("17");
+            comboBoxDias.Items.Add("18");
+            comboBoxDias.Items.Add("19");
+            comboBoxDias.Items.Add("20");
+            comboBoxDias.Items.Add("21");
+            comboBoxDias.Items.Add("22");
+            comboBoxDias.Items.Add("23");
+            comboBoxDias.Items.Add("24");
+            comboBoxDias.Items.Add("25");
+            comboBoxDias.Items.Add("26");
+            comboBoxDias.Items.Add("27");
+            comboBoxDias.Items.Add("28");
+            comboBoxDias.Items.Add("29");
+            comboBoxDias.Items.Add("30");
+            comboBoxDias.Items.Add("31");
+            // aqui de clare el comboboxdias para que el usuario no mas ponga el dia 
+        }
+
+        private void comboBoxsig_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOPTENER_Click(object sender, EventArgs e)
+        {
+            if (comboBoxDias.SelectedItem == null || comboBoxMes.SelectedIndex == -1)//en la condicion del if primero vaciamos comboboxdias
+            {
+                MessageBox.Show("Seleccione un día y un mes.");
+                return;
+            }
+
+            // Convertir valores de los ComboBox
+            int dia = int.Parse(comboBoxDias.SelectedItem.ToString());// aqui de clare una variable con int dia para comboboxdias 
+            int mes = comboBoxMes.SelectedIndex + 1; // Convertir el comboboxmes en int mes para que escoja
+
+            string signo = "";
+
+            if ((mes == 3 && dia >= 21) || (mes == 4 && dia <= 19))
+                signo = "Aries";
+            else if ((mes == 4 && dia >= 20) || (mes == 5 && dia <= 20))
+                signo = "Tauro";
+            else if ((mes == 5 && dia >= 21) || (mes == 6 && dia <= 20))
+                signo = "Géminis";
+            else if ((mes == 6 && dia >= 21) || (mes == 7 && dia <= 22))
+                signo = "Cáncer";
+            else if ((mes == 7 && dia >= 23) || (mes == 8 && dia <= 22))
+                signo = "Leo";
+            else if ((mes == 8 && dia >= 23) || (mes == 9 && dia <= 22))
+                signo = "Virgo";
+            else if ((mes == 9 && dia >= 23) || (mes == 10 && dia <= 22))
+                signo = "Libra";
+            else if ((mes == 10 && dia >= 23) || (mes == 11 && dia <= 21))
+                signo = "Escorpio";
+            else if ((mes == 11 && dia >= 22) || (mes == 12 && dia <= 21))
+                signo = "Sagitario";
+            else if ((mes == 12 && dia >= 22) || (mes == 1 && dia <= 19))
+                signo = "Capricornio";
+            else if ((mes == 1 && dia >= 20) || (mes == 2 && dia <= 18))
+                signo = "Acuario";
+            else if ((mes == 2 && dia >= 19) || (mes == 3 && dia <= 20))
+                signo = "Piscis"; // aqui de clare los meses y dias que debe ser los sodiacos con lo que puso en las condiciones 
+            else
+                signo = "checa bien la fecha";// esto lo puse porsi algo sale mal 
+
+            // Mostrar resultado en el TextBox
+            textBoxZodi.Text = "Tu signo zodiacal es: " + signo;
+
+        }
+
+        private void textBoxZodi_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
